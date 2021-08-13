7a43c1c1e6510ce79fb7cab188297d17f5437e71
mason roberts <mrobe18@gmail.com>
2021-08-13 01:41:54 -0400

    created base server, route and controller for auth, register

--- server/server.js
+++ server/server.js
@@ -1,1 +1,22 @@
-//this is where the server goes.
+import express from 'express';
+import fs from 'fs';
+import { readdirSync } from 'fs';
+import cors from 'cors';
+const morgan = require('morgan');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+readdirSync('./routes').map((rout) =>
+  app.use('/api', require(`./routes/${rout}`))
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`server is listening in ${process.env.MODE} on port ${PORT}`)
+);
